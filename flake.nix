443979957088963282e6cb6e1fd6b9a4626bdd86
paradoxical-dev <jaredonnell21@gmail.com>
2024-11-01 19:26:02 -0500

    initial flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "base configuration";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {self, nixpkgs, home-manager, ...}: 
+  let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations = {
+      main =lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+     gitmoney = home-manager.lib.homeManagerConfigurations {
+      inherit pkgs;
+      modules = [ ./home-manager.nix ]
+     };
+    };
+  };
+}
--- home-manager.nix
+++ home-manager.nix
@@ -1,12 +1,6 @@
 {config, pkgs, ...}:
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
-in
-{
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
 
+{
   home-manager.users.gitmoney = {
     home.stateVersion = "24.05";
 
@@ -18,64 +12,64 @@ in
       syntaxHighlighting.enable = true;
 
       shellAliases = {
-	update = "sudo nixos-rebuild switch";
+	      update = "sudo nixos-rebuild switch";
 
         c = "clear";
-	ff = "fastfetch";
-	ls = "eza -a --icons";
-	ll = "eza -al --icons";
-	lt = "eza -a --tree --level=1 --icons";
-	shutdown = "systemctl poweroff";
-	v = "nvim";
-	wifi = "nmtui";
+	      ff = "fastfetch";
+	      ls = "eza -a --icons";
+	      ll = "eza -al --icons";
+	      lt = "eza -a --tree --level=1 --icons";
+	      shutdown = "systemctl poweroff";
+	      v = "nvim";
+	      wifi = "nmtui";
 
-	gs = "git status";
-	ga = "git add";
-	gc = "git commit -m";
-	gp = "git push";
-	gpl = "git pull";
-	gst = "git stash";
-	gsp = "git stash; git pull";
-	gcheck = "git checkout";
-	gcredential = "git config credential.helper store";
+	      gs = "git status";
+	      ga = "git add";
+	      gc = "git commit -m";
+	      gp = "git push";
+	      gpl = "git pull";
+	      gst = "git stash";
+	      gsp = "git stash; git pull";
+	      gcheck = "git checkout";
+	      gcredential = "git config credential.helper store";
       };
 
       history = {
-	size = 10000;
-	path = "/home/gitmoney/.zsh_history";
+	      size = 10000;
+	      path = "/home/gitmoney/.zsh_history";
       };
 
       oh-my-zsh = {
-	enable = true;
-	plugins = [
-	  "git"
-	  "sudo"
-	  "web-search"
-	  "copyfile"
-	  "copybuffer"
-          "dirhistory"
-	];
+	      enable = true;
+	      plugins = [
+	        "git"
+	          "sudo"
+	          "web-search"
+	          "copyfile"
+	          "copybuffer"
+            "dirhistory"
+	      ];
       };
 
       initExtra = ''
         # Autostart
-	if [[ $(tty) == *"pts"* ]]; then
-    	  fastfetch
-	else
+	      if [[ $(tty) == *"pts"* ]]; then
+    	    fastfetch
+	      else
     	  echo
-    	  if [ -f /bin/qtile ]; then
+    	    if [ -f /bin/qtile ]; then
             echo "Start Qtile X11 with command Qtile"
-    	  fi
-    	  if [ -f /bin/hyprctl ]; then
+    	    fi
+    	    if [ -f /bin/hyprctl ]; then
             echo "Start Hyprland with command Hyprland"
-    	  fi
-	fi
+    	    fi
+	      fi
 
-	#Starship
-	eval "$(starship init zsh)"
+	      #Starship
+	      eval "$(starship init zsh)"
 
-	#Fzf
-	source <(fzf --zsh)
+	      #Fzf
+	      source <(fzf --zsh)
       '';
     };
 
