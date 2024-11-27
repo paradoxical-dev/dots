dd25a06f912c51c85e84b013b968646cd6606ee6
paradoxical-dev <jaredonnell21@gmail.com>
2024-11-26 20:32:14 -0600

    hyprland refactor

--- system/wm/hypr/hyprland.nix
+++ system/wm/hypr/hyprland.nix
@@ -6,8 +6,12 @@
       systemd.enable = true;
       xwayland.enable = true;
       extraConfig = ''
-      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/keybindings.conf
-      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/themes/${userSettings.theme}.conf
+      source = ~/dots/system/wm/hypr/conf/settings/keybindings.conf
+      source = ~/dots/system/wm/hypr/conf/settings/inputs.conf
+      source = ~/dots/system/wm/hypr/conf/settings/env.conf
+      source = ~/dots/system/wm/hypr/conf/settings/windowrule.conf
+      source = ~/dots/system/wm/hypr/conf/settings/apps.conf
+      source = ~/dots/system/wm/hypr/conf/themes/${userSettings.theme}.conf
       '';
     };
   };
