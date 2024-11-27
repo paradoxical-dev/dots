6d891a4b55b2b376d7dae0f714589d42189f2857
paradoxical-dev <jaredonnell21@gmail.com>
2024-11-26 20:58:06 -0600

    hyprland fix

--- system/wm/hypr/hyprland.nix
+++ system/wm/hypr/hyprland.nix
@@ -6,12 +6,12 @@
       systemd.enable = true;
       xwayland.enable = true;
       extraConfig = ''
-      source = ~/dots/system/wm/hypr/conf/settings/keybindings.conf
-      source = ~/dots/system/wm/hypr/conf/settings/inputs.conf
-      source = ~/dots/system/wm/hypr/conf/settings/env.conf
-      source = ~/dots/system/wm/hypr/conf/settings/windowrule.conf
-      source = ~/dots/system/wm/hypr/conf/settings/apps.conf
-      source = ~/dots/system/wm/hypr/conf/themes/${userSettings.theme}.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/settings/apps.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/settings/env.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/settings/inputs.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/settings/keybindings.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/themes/${userSettings.theme}.conf
+      source = ${config.home.homeDirectory}dots/system/wm/hypr/conf/settings/windowrule.conf
       '';
     };
   };
