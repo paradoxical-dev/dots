2db230964bbdb532ad71971f0ce06e68e4bb2a61
paradoxical-dev <jaredonnell21@gmail.com>
2024-12-06 17:42:05 -0600

    hyprpaper fix

--- system/wm/hypr/hyprpaper/default.nix
+++ system/wm/hypr/hyprpaper/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, userSettings, ... }:
+{ config, pkgs, lib, userSettings, ... }:
 let
   themes = {
     yin = "${config.home.homeDirectory}dots/wallpapers/yin.png";
@@ -16,6 +16,14 @@ in
         wallpaperPath
       ];
       wallpaper = monitorWallpapers;
+    };
+  };
+  # Service override fix
+  systemd.user.services.hyprpaper = {
+    Service = {
+      # provide a delay and for some reason it works
+      # 0.5 seconds is the lowest I could get to work
+      ExecStartPre = "${pkgs.coreutils}/bin/sleep 0.5";
     };
   };
 }
