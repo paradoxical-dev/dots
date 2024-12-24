25d6bb4fc5df86016efb135daf51ab2a84897a95
paradoxical-dev <jaredonnell21@gmail.com>
2024-12-24 01:30:30 -0600

    Update: wofi rework continued

--- user/de/hypr/wofi/default.nix
+++ user/de/hypr/wofi/default.nix
@@ -1,16 +1,15 @@
 { config, pkgs, userSettings, ... }:
-let
-  currentTheme = import ./themes/${userSettings.theme}.nix;
-in
 {
   programs.wofi = {
     enable = true;
     settings = {
+      width = "400px";
+      height = "450px";
       allow_markup = true;
       allow_images = true;
+      stylesheet = builtins.toPath "${config.home.homeDirectory}dots/user/de/hypr/wofi/themes/${userSettings.theme}.css";
       term = userSettings.terminal;
       prompt = "where we droppin...";
     };
-    style = currentTheme.config;
   };
 }
